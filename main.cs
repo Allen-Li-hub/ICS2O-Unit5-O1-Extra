1623cdd8bde8ff5286033b584b2063fe639cd630
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-11-25 16:59:30 -0500

    change

--- main.cs
+++ main.cs
@@ -1,7 +1,6 @@
-// Created by: Allen Li
-// Created on: Oct 2022
+// Created by: Allen li
+// Created on: Nov 2022
 //
-// This program displays, "Hello, World!"
 
 using System;
 
@@ -9,7 +8,30 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int userNumber;
+        int randomNumber;
+
+        Console.WriteLine("Can you guess the random number from 1-6?");
+        Console.WriteLine("");
+
+        Console.Write("Enter random value from 1-6: ");
+        Console.WriteLine("");
+        userNumber = Convert.ToInt16(Console.ReadLine());
+
+        Random rnd = new Random();
+        randomNumber = rnd.Next(1, 6);
+
+        Console.WriteLine("");
+        if (userNumber == randomNumber)
+        {
+            Console.WriteLine("The answer was: " + randomNumber + ". You got it right!");
+        }
+
+        if (userNumber != randomNumber)
+        {
+            Console.WriteLine("The answer was: " + randomNumber + ". Your answer was wrong. Please try again!");
+        }
+
         Console.WriteLine("\nDone.");
     }
 }
